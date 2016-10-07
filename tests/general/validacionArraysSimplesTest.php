365496172a16f43b7a29a7c35e293c2fd892252f
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com>
2016-10-07 20:30:44 +0200

    Añadidos más tests de arrays simples

--- tests/general/validacionArraysSimplesTest.php
+++ tests/general/validacionArraysSimplesTest.php
@@ -12,4 +12,27 @@ class validacionArraysSimplesTest extends PHPTest_Unit {
 		$this->assertEquals($input, $output);
 	}
 
+	public function testArrayNumeros() {
+		$input = array(1, 2, 3);
+
+		$output = array_keys_php($input);
+
+		$this->assertEquals($input, $output);
+	}
+
+	public function testArrayCadenas() {
+		$input = array('a', 'b', 'c');
+
+		$output = array_keys_php($input);
+
+		$this->assertEquals($input, $output);
+	}
+
+	public function testArrayMixed() {
+		$input = array('a', 2, 'c');
+
+		$output = array_keys_php($input);
+
+		$this->assertEquals($input, $output);
+	}
 }
