9d61cc969e684f36a4c28359e9bbfa5ab6c097b9
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com>
2016-10-07 21:45:17 +0200

    Refactorizando función para simplificarla

--- src/array_keys_php.php
+++ src/array_keys_php.php
@@ -11,6 +11,20 @@
 */
 function array_keys_php($array, $search_value = null, $strict = false) {
 
+	$seEstaRealizandoUnaBusqueda = function($numArgumentos) {
+		return $numArgumentos > 1;
+	};
+
+	$esIgualValorBusqueda = function($valor) use ($search_value, $strict) {
+		if(!$strict && $valor == $search_value) {
+			return true;
+		} else if($strict && $valor === $search_value) {
+			return true;
+		}
+
+		return false;
+	};
+
  	$validarParametrosEntrada = function() use ($array, $search_value, $strict) {
 		if(!is_array($array)) {
     		trigger_error('Warning: array_keys() expects parameter 1 to be array, ' .
@@ -36,12 +50,9 @@ function array_keys_php($array, $search_value = null, $strict = false) {
  	$output = array();
 
  	foreach($array as $key => $value) {
- 		/* Si se pasa más de un parámetro se está realizando una búsqueda */
- 		if(func_num_args() > 1) {
- 			if(!$strict && $array[$key] == $search_value) {
- 				array_push($output, $key);
- 			} else if($strict && $array[$key] === $search_value) {
- 				array_push($output, $key);
+ 		if($seEstaRealizandoUnaBusqueda(func_num_args())) {
+ 			if($esIgualValorBusqueda($array[$key])) {
+				array_push($output, $key);
  			}
  		} else {
  			array_push($output, $key);
