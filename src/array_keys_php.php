6d7c793e1c658907eefb85d2f13d96cb1d099b37
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com>
2016-10-07 20:12:49 +0200

    Añadimos validación de que el primer parámetro sea un array

--- src/array_keys_php.php
+++ src/array_keys_php.php
@@ -1,2 +1,23 @@
 <?php
 	
+
+	 function array_keys_php ($array ,$search_value = null ,$strict = false) {
+
+	 	function parametrosEntradaValidos($array , $search_value , $strict) {
+	 		if(!is_array($array)) {
+		 		$type = gettype($array);
+
+	    		trigger_error('Warning: array_keys() expects parameter 1 to be array, ' .
+	    				gettype($array) . ' given', E_USER_WARNING);
+
+		 		return false;
+		 	}
+
+	 	}
+
+	 	if(!parametrosEntradaValidos($array, $search_value, $strict)) {
+	 		return null;
+	 	}
+
+	 	return null;
+	 }
---
+++ tests/general/validacionParametrosEntradaTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+require_once dirname(__FILE__) . '/../PHPTest_Unit.php';
+
+class validacionParametrosEntradaTest extends PHPTest_Unit {
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error_Warning
+	*/
+	public function testLanzarErrorSiNoHayParametros() {
+		array_keys_php();
+	}
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error_Warning
+	*/
+	public function testLanzarErrorSiPrimerParametroNoEsArray() {
+		array_keys_php(null);
+		array_keys_php("");
+	}
+}
