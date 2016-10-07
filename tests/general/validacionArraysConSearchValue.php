177f76dc752f3174ccb8c3e4b65678dbf8b3367c
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com>
2016-10-07 21:24:36 +0200

    Añadidos tests para búsquedas de arrays

--- tests/general/validacionArraysConSearchValue.php
+++ tests/general/validacionArraysConSearchValue.php
@@ -21,4 +21,49 @@ class validacionArraysConSearchValue extends PHPTest_Unit {
 
 		$this->assertEquals($expected, $output);
 	}
+
+	public function testSearchStringNoStrict() {
+		$input = array(1=>1, 2=>2, array());
+
+		$output = array_keys_php($input, 'a', false);
+		$expected = array();
+
+		$this->assertEquals($expected, $output);
+	}
+
+	public function testSearchStringStrict() {
+		$input = array(1=>1, 2=>2, array(), 'a');
+
+		$output = array_keys_php($input, 'a', true);
+		$expected = array(4);
+
+		$this->assertEquals($expected, $output);
+	}
+
+	public function testSearchArrayNoStrict() {
+		$input = array(1=>1, 2=>2, array(), 'a');
+
+		$output = array_keys_php($input, array(), false);
+		$expected = array(3);
+
+		$this->assertEquals($expected, $output);
+	}
+
+	public function testSearchArrayConValorNoStrict() {
+		$input = array(1=>1, 2=>2, array(), 'a');
+
+		$output = array_keys_php($input, array(1), false);
+		$expected = array();
+
+		$this->assertEquals($expected, $output);
+	}
+
+	public function testSearchArrayConValorNoStrictDos() {
+		$input = array(1=>1, 2=>2, array(1), 'a');
+
+		$output = array_keys_php($input, array(1), false);
+		$expected = array(3);
+
+		$this->assertEquals($expected, $output);
+	}
 }
